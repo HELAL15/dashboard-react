b160937bfb716df1520a1ce0b05902c677174f00
HELAL15 <ah4459577@gmail.com>
2024-11-21 17:02:52 +0200

    fix

--- src/pages/subCategories/SubCategories.tsx
+++ src/pages/subCategories/SubCategories.tsx
@@ -1,9 +1,9 @@
 import { FC, memo } from "react";
-import { Link, useLocation } from "react-router-dom";
-import {  TableColumnsType } from "antd";
+import { Link } from "react-router-dom";
+// import {  TableColumnsType } from "antd";
 import SecTitle from "../../components/global/SecTitle";
-import CustomTable from "../../components/CustomTable";
-import useFetch from "../../hooks/useFetch";
+
+// import useFetch from "../../hooks/useFetch";
 
 /**
  * ==> props interface
@@ -19,43 +19,43 @@ const SubCategories: FC<IProps> = () => {
   // const lang = i18n.language
 
 
-  const location = useLocation();
+  // const location = useLocation();
 
   
-  const searchParams = new URLSearchParams(location.search);
-  const parentId = searchParams.get('parent_id');
+  // const searchParams = new URLSearchParams(location.search);
+  // const parentId = searchParams.get('parent_id');
 
-  const { data, isLoading, refetch } = useFetch(`categorys?parent_id=${parentId}` );
+  // const { data, isLoading, refetch } = useFetch(`categorys?parent_id=${parentId}` );
 
-  console.log(data);
+  // console.log(data);
   
 
   
-  const columns: TableColumnsType = [
+  // const columns: TableColumnsType = [
 
-    {
-      title: "title",
-      dataIndex: `title`,
-      align: "center",
-      responsive: ["xs", "sm", "md", "lg"],
-    },
+  //   {
+  //     title: "title",
+  //     dataIndex: `title`,
+  //     align: "center",
+  //     responsive: ["xs", "sm", "md", "lg"],
+  //   },
 
-    {
-      title: "image",
-      dataIndex: "image",
-      align: "center",
-      responsive: ["xs", "sm", "md", "lg"],
-    },
+  //   {
+  //     title: "image",
+  //     dataIndex: "image",
+  //     align: "center",
+  //     responsive: ["xs", "sm", "md", "lg"],
+  //   },
 
-    {
-      title: "sub categories",
-      dataIndex: `viewSubCategorys`,
-      align: "center",
-      responsive: ["xs", "sm", "md", "lg"],
-    },
+  //   {
+  //     title: "sub categories",
+  //     dataIndex: `viewSubCategorys`,
+  //     align: "center",
+  //     responsive: ["xs", "sm", "md", "lg"],
+  //   },
     
 
-  ];
+  // ];
   return (
     <>
     <section>
@@ -67,13 +67,13 @@ const SubCategories: FC<IProps> = () => {
             Add category
           </Link>
         </div>
-        <CustomTable 
+        {/* <CustomTable 
           data={data}
           isLoading={isLoading}
           refetch={refetch}
           deleteEndPoint="categorys" 
           cols={columns} 
-          />
+          /> */}
       </div>
     </section>
             </>
